5f4649455b43f18b487dd11f8ca6293d9e255600
Angus Perkerson <angusp@angusp.com>
2016-08-01 22:08:46 -0700

    Adds mock api to react pettiest

--- react-petlist/build/webpack-dev-server.js
+++ react-petlist/build/webpack-dev-server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
+import fs from 'fs';
 
 import config from '../config';
 import webpackConfig from './dev.config.babel';
@@ -18,13 +19,45 @@ const serverOptions = {
   inline: true,
   lazy: false,
   publicPath: webpackConfig.output.publicPath,
-  stats: {colors: true}
+  stats: {colors: true},
 };
+
+function readJsonFileSync(filepath, encoding){
+
+    if (typeof (encoding) == 'undefined'){
+        encoding = 'utf8';
+    }
+    var file = fs.readFileSync(filepath, encoding);
+    return JSON.parse(file);
+}
 
 const middleware = webpackMiddleware(compiler, serverOptions);
 app.use(middleware);
 app.use(webpackHotMiddleware(compiler));
 app.use('/static', express.static('./src/web-api'));
+
+app.use('/api/search', function(req, res) {
+
+  const service = req.query.service;
+  let file = 'search.json';
+
+  if(service) {
+    file = `search-${service}.json`;
+  }
+
+  let json = {
+    search: []
+  };
+
+  try {
+    json = readJsonFileSync(`./src/web-api/${file}`);
+  } catch(err) {
+    res.status(404);
+  }
+
+  res.json(json);
+
+});
 
 app.listen(config.server_port, function onAppListening(err) {
   if (err) {
